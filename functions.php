40d5f3369505f416550bffd11f7e79659e5f95c0
Dmitryfor <dimaportnov2000@gmail.com>
2024-03-19 20:59:11 +0200

    composer update, remove features folder

--- functions.php
+++ functions.php
@@ -54,7 +54,7 @@ if (! function_exists('\Roots\bootloader')) {
 |
 */
 
-collect(['setup', 'filters','core', 'scripts', 'ssm'])
+collect(['setup', 'filters', 'core', 'scripts', 'ssm'])
     ->each(function ($file) {
         if (! locate_template($file = "app/{$file}.php", true, true)) {
             wp_die(
